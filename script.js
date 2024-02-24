0a5ca9911557d626fbf116b8865f82d4df3ad5c2
Favour A. Uzochukwu <uzochukwu.favour310@gmail.com>
2024-02-25 00:16:42 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+	//Setting the deadline for the countdown date in milliseconds
+	let countDownDate = new Date("2024-03-02:11:30").getTime();
+
+	let countDown = setInterval(() => {
+
+		const currentDate = new Date().getTime();
+
+		const timeLeft = countDownDate - currentDate;
+
+		const dayLeft = Math.floor(timeLeft / (1000 * 24 * 60 * 60));
+		const monthLeft = Math.floor(timeLeft % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+		const minutesLeft = Math.floor(timeLeft % (1000 * 60 * 60)/ (1000 * 60));
+		const secondsLeft = Math.floor(timeLeft % (1000 * 60) / 1000);
+
+		$('.day_digit').text(dayLeft);
+		$('.hour_digit').text(monthLeft);
+		$('.minute_digit').text(minutesLeft);
+		$('.seconds_digit').text(secondsLeft);
+
+
+		if(timeLeft < 0) {
+			clearInterval(countDown);
+		$('.timer').html('<span class="text-red-500 font-bold text-3xl text-center">Giveaway Has Expired!</span>');
+		}
+	}, 1000);
+
+})
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'background-colour': '#bcccdc',
+        'text-color': '#0b64a4',
+        'paragraph-color': '#48647f;',
+        'timer-color': '#063251'
+      }
+    },
+  },
+  plugins: [],
+}
+
