c6c6474965ed57075bbd9c8de0f31dca2f34c955
Favour A. Uzochukwu <uzochukwu.favour310@gmail.com>
2024-02-29 08:47:40 +0100

    Correction of NaN error inthe countdown on ios devices and fixing of image responsivesnes using tailwindCSS

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
 $(document).ready(function () {
-	//Setting the deadline for the countdown date in milliseconds
-	let countDownDate = new Date("2024-04-04:11:30").getTime();
+	// Setting the deadline for the countdown date in milliseconds
+	let countDownDate = new Date("2024-04-04T11:30:00").getTime();
 
 	let countDown = setInterval(() => {
+			const currentDate = new Date().getTime();
+			const timeLeft = countDownDate - currentDate;
 
-		const currentDate = new Date().getTime();
+			const dayLeft = Math.floor(timeLeft / (1000 * 24 * 60 * 60));
+			const monthLeft = Math.floor(timeLeft % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+			const minutesLeft = Math.floor(timeLeft % (1000 * 60 * 60) / (1000 * 60));
+			const secondsLeft = Math.floor(timeLeft % (1000 * 60) / 1000);
 
-		const timeLeft = countDownDate - currentDate;
+			$('.day_digit').text(dayLeft);
+			$('.hour_digit').text(monthLeft);
+			$('.minute_digit').text(minutesLeft);
+			$('.seconds_digit').text(secondsLeft);
 
-		const dayLeft = Math.floor(timeLeft / (1000 * 24 * 60 * 60));
-		const monthLeft = Math.floor(timeLeft % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
-		const minutesLeft = Math.floor(timeLeft % (1000 * 60 * 60)/ (1000 * 60));
-		const secondsLeft = Math.floor(timeLeft % (1000 * 60) / 1000);
-
-		$('.day_digit').text(dayLeft);
-		$('.hour_digit').text(monthLeft);
-		$('.minute_digit').text(minutesLeft);
-		$('.seconds_digit').text(secondsLeft);
-
-
-		if(timeLeft < 0) {
-			clearInterval(countDown);
-		$('.timer').html('<span class="text-red-500 font-bold text-3xl text-center">Giveaway Has Expired!</span>');
-		}
+			if (timeLeft < 0) {
+					clearInterval(countDown);
+					$('.timer').html('<span class="text-red-500 font-bold text-3xl text-center">Giveaway Has Expired!</span>');
+			}
 	}, 1000);
-
-})
+});
